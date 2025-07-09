9e4be10361e11120613e13bf5af49bfe7cfb5840
sxhilx <jadasahil786@gmail.com>
2025-07-09 12:50:31 +0200

    feat: add about and 404 page and a footer component

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const navItems = [
   { href: '/', label: 'Home' },
-  { href: '/about', label: 'About' },
   { href: '/writing', label: 'Writing' },
+  { href: '/about', label: 'About' },
 ];
